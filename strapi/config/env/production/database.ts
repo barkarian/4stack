2ff49d9d2874f313e84f9eb8319b114f22313af7
barkarian <thodorisbakas@gmail.com>
2024-01-25 18:49:53 +0200

    env

--- strapi/config/env/production/database.ts
+++ strapi/config/env/production/database.ts
@@ -1,9 +1,17 @@
 module.exports = ({ env }) => ({
-    connection: {
-        client: 'postgres',
-        connection: {
-            connectionString: env('DATABASE_PRIVATE_URL')
-        },
-        pool: { min: 0 }
+    defaultConnection: 'default',
+    connections: {
+        default: {
+            connector: 'bookshelf',
+            settings: {
+                client: 'postgres',
+                host: env('DATABASE_HOST'),
+                port: env('DATABASE_PORT'),
+                database: env('DATABASE_NAME'),
+                username: env('DATABASE_USERNAME'),
+                password: env('DATABASE_PASSWORD'),
+            },
+            options: {}
+        }
     }
 });
