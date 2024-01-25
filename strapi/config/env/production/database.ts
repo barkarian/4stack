f05b84ca7ce556b599b00e6efa3f2f98047f2ebe
barkarian <thodorisbakas@gmail.com>
2024-01-25 19:12:19 +0200

    try only with dburl (which contains all the infos)

--- strapi/config/env/production/database.ts
+++ strapi/config/env/production/database.ts
@@ -5,11 +5,12 @@ module.exports = ({ env }) => ({
             connector: 'bookshelf',
             settings: {
                 client: 'postgres',
-                host: env('DATABASE_HOST'),
-                port: env('DATABASE_PORT'),
-                database: env('DATABASE_NAME'),
-                username: env('DATABASE_USERNAME'),
-                password: env('DATABASE_PASSWORD'),
+                url: env('DATABASE_URL'),
+                // host: env('DATABASE_HOST'),
+                // port: env('DATABASE_PORT'),
+                // database: env('DATABASE_NAME'),
+                // username: env('DATABASE_USERNAME'),
+                // password: env('DATABASE_PASSWORD'),
             },
             options: {}
         }
