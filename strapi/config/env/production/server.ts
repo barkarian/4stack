e896c1987abab80a2adaf3d7866ed9e868829d0e
barkarian <thodorisbakas@gmail.com>
2024-01-26 19:14:50 +0200

    Set public strapi url env

--- strapi/config/env/production/server.ts
+++ strapi/config/env/production/server.ts
@@ -1,7 +1,7 @@
 
 module.exports = ({ env }) => ({
     proxy: true,
-    url: env('APP_URL'), // Sets the public URL of the application.
+    url: env('PUBLIC_STRAPI_URL', 'http://localhost:1337'),
     app: {
         keys: env.array('APP_KEYS')
     },
