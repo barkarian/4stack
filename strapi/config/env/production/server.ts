c089e0d4721dcf29b0acda593b3753fa21e9814f
barkarian <thodorisbakas@gmail.com>
2024-01-26 19:19:39 +0200

    url->domain

--- strapi/config/env/production/server.ts
+++ strapi/config/env/production/server.ts
@@ -1,7 +1,7 @@
 
 module.exports = ({ env }) => ({
     proxy: true,
-    url: env('PUBLIC_STRAPI_URL', 'http://localhost:1337'),
+    url: env('PUBLIC_STRAPI_DOMAIN', 'http://localhost:1337'),
     app: {
         keys: env.array('APP_KEYS')
     },
