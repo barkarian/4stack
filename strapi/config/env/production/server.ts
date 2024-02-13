bdf9e3b1c0a8b283e2ef139349668a53fe1c6e21
barkarian <thodorisbakas@gmail.com>
2024-02-13 17:16:53 +0200

    test

--- strapi/config/env/production/server.ts
+++ strapi/config/env/production/server.ts
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
-    proxy: true,
-    // url: env('PUBLIC_STRAPI_DOMAIN'), // Sets the public URL of the application.
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
     app: {
-        keys: env.array('APP_KEYS')
+        keys: env.array('APP_KEYS'),
     },
+    url: env('PUBLIC_STRAPI_DOMAIN'),
+    proxy: true
 });
