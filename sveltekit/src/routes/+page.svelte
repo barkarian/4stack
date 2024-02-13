83769cc55430cb40f1a43af1f4003dd01d822fcb
barkarian <thodorisbakas@gmail.com>
2024-02-13 19:59:18 +0200

    minor

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to 4Stack</h1>
+<p>A fast, scalable and easy way to develop and deploy with Strapi, Sveltekit.</p>
