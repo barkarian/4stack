86704d27578dad6e0d71f23ddd85578d983296ff
barkarian <thodorisbakas@gmail.com>
2024-02-22 13:21:19 +0200

    Fix openapi generation on strapi... by adding "lib": ["dom", "esnext"] to tsconfig

---
+++ strapi/src/openapi/beds24/codegen/apis/index.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AccountsApi';
+export * from './AuthenticationApi';
+export * from './BookingsApi';
+export * from './ChannelsAirbnbApi';
+export * from './ChannelsBookingComApi';
+export * from './ChannelsSettingsApi';
+export * from './ChannelsStripeApi';
+export * from './InventoryApi';
+export * from './PropertiesApi';
+export * from './WebhooksApi';
